3a54e0ce749e0e463062bc372ab290f77a751f44
ivan.turovsky <ivan.turovsky@superbbit.com>
2023-08-22 15:06:45 +0300

    add VAccordion, add ComponentsAccordionPage

---
+++ src/components/basic/VAccordion.vue
@@ -0,0 +1,131 @@
+<template>
+  <div
+    :class="[
+      $style.root,
+      isOpen && $style.open
+    ]"
+    @click="isOpen = !isOpen"
+  >
+    <div :class="$style.visible">
+      <h6 :class="$style.title">
+        {{ props.title }}
+      </h6>
+
+      <div :class="$style.icon">
+        <div />
+        <div />
+      </div>
+    </div>
+
+    <div :class="$style.desc">
+      <div ref="desc">
+        <slot />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import {
+  onMounted,
+  ref,
+  watch,
+} from 'vue';
+
+interface PropsType {
+  name?: string;
+  title?: string;
+}
+
+interface EmitsType {
+  (e: 'update:name', value: string): void;
+}
+
+const props = withDefaults(defineProps<PropsType>(), {
+  title: 'Untitled',
+  name: undefined,
+});
+const emits = defineEmits<EmitsType>();
+
+const desc = ref<HTMLDivElement | null>(null);
+const descHeight = ref<string>('0px');
+const isOpen = ref<boolean>(false);
+
+onMounted(() => {
+  if (desc?.value) {
+    descHeight.value = `${desc.value.getBoundingClientRect().height}px`;
+  }
+});
+
+watch(() => isOpen.value, () => {
+  if (isOpen.value) {
+    emits('update:name', props.title);
+  }
+});
+
+watch(() => props.name, () => {
+  if (props.name !== undefined && props.name !== props.title) {
+    isOpen.value = false;
+  }
+});
+</script>
+
+<style module lang="scss">
+@import '@/scss/mixins/typography';
+
+.root {
+  padding: 10px 25px;
+  background-color: var(--color-card);
+  width: 100%;
+  border-radius: 15px;
+  cursor: pointer;
+  border: 1px solid var(--color-border);
+}
+
+.desc {
+  --width-desc: v-bind(descHeight);
+
+  overflow: hidden;
+  transition: height 0.2s,
+    margin 0.2s;
+  height: 0;
+
+  .open & {
+    height: var(--width-desc);
+    margin-top: 10px;
+  }
+}
+
+.visible {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+
+.title {
+  @include heading6;
+}
+
+.icon {
+  position: relative;
+  height: 15px;
+
+  & div:nth-child(1),
+  & div:nth-child(2) {
+    position: absolute;
+    background-color: var(--color-text);
+    width: 2px;
+    height: 15px;
+    transition: transform 0.1s;
+  }
+
+  & div:nth-child(2) {
+    transform: rotateZ(90deg);
+  }
+
+  .open & div:nth-child(1),
+  .open & div:nth-child(2) {
+    transform: rotateZ(270deg);
+  }
+}
+</style>
---
+++ src/views/protected/components/ComponentsAccordionPage.vue
@@ -0,0 +1,75 @@
+<template>
+  <div :class="$style.root">
+    <VPreviewComponentCard
+      title="Basic accordion"
+      desc="Use <VAccordion> components"
+      transparent
+    >
+      <VAccordion title="Accordion 1">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore ipsa quam rem. Earum nobis omnis rem
+        unde. Consectetur consequuntur exercitationem in laboriosam, magni neque numquam provident quod saepe sed tenetur!
+      </VAccordion>
+      <VAccordion title="Accordion 2">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore ipsa quam rem. Earum nobis omnis rem
+        unde. Consectetur consequuntur exercitationem in laboriosam, magni neque numquam provident quod saepe sed tenetur!
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore ipsa quam rem. Earum nobis omnis rem
+        unde. Consectetur consequuntur exercitationem in laboriosam, magni neque numquam provident quod saepe sed tenetur!
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore ipsa quam rem. Earum nobis omnis rem
+        unde. Consectetur consequuntur exercitationem in laboriosam, magni neque numquam provident quod saepe sed tenetur!
+      </VAccordion>
+      <VAccordion title="Accordion 3">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore ipsa quam rem. Earum nobis omnis rem
+        unde. Consectetur consequuntur exercitationem in laboriosam, magni neque numquam provident quod saepe sed tenetur!
+      </VAccordion>
+    </VPreviewComponentCard>
+
+    <VPreviewComponentCard
+      title="Linked Accordions"
+      desc="To make linked accordions, you need to create a variable and bind it via «v-model:name» to each accordion to be linked"
+      transparent
+    >
+      <VAccordion
+        v-model:name="linkedAccordion"
+        title="Accordion 1"
+      >
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore ipsa quam rem. Earum nobis omnis rem
+        unde. Consectetur consequuntur exercitationem in laboriosam, magni neque numquam provident quod saepe sed tenetur!
+      </VAccordion>
+      <VAccordion
+        v-model:name="linkedAccordion"
+        title="Accordion 2"
+      >
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore ipsa quam rem. Earum nobis omnis rem
+        unde. Consectetur consequuntur exercitationem in laboriosam, magni neque numquam provident quod saepe sed tenetur!
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore ipsa quam rem. Earum nobis omnis rem
+        unde. Consectetur consequuntur exercitationem in laboriosam, magni neque numquam provident quod saepe sed tenetur!
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore ipsa quam rem. Earum nobis omnis rem
+        unde. Consectetur consequuntur exercitationem in laboriosam, magni neque numquam provident quod saepe sed tenetur!
+      </VAccordion>
+      <VAccordion
+        v-model:name="linkedAccordion"
+        title="Accordion 3"
+      >
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore ipsa quam rem. Earum nobis omnis rem
+        unde. Consectetur consequuntur exercitationem in laboriosam, magni neque numquam provident quod saepe sed tenetur!
+      </VAccordion>
+    </VPreviewComponentCard>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+
+import VAccordion from '@/components/basic/VAccordion.vue';
+import VPreviewComponentCard from '@/components/basic/VPreviewComponentCard.vue';
+
+const linkedAccordion = ref<string>('');
+</script>
+
+<style module lang="scss">
+@import "@/scss/mixins/mixins";
+
+.root {
+  @include grid;
+}
+</style>
