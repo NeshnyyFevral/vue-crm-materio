d170cc43f62a50fcddeb133ec5b1c922de98adb0
ivan.turovsky <ivan.turovsky@superbbit.com>
2023-08-31 09:53:24 +0300

    add dosc for github pages

---
+++ docs/assets/FormComponentsPage.854a2cc4.js
@@ -0,0 +1,1 @@
+import{_ as o,q as r,b as n,x as t}from"./index.ddb1da35.js";const c={};function s(_,a){const e=r("router-view");return n(),t(e)}var p=o(c,[["render",s]]);export{p as default};
---
+++ docs/assets/TypographyPage.bb3b30fc.js
@@ -0,0 +1,1 @@
+import{_ as o,q as r,b as t,x as c}from"./index.ddb1da35.js";const n={};function a(_,s){const e=r("router-view");return t(),c(e)}var f=o(n,[["render",a]]);export{f as default};
