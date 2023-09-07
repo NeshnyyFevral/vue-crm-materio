0b4e4e105d723169cc89bac2bbea3679fe6a1257
ivan.turovsky <ivan.turovsky@superbbit.com>
2023-09-07 12:01:27 +0300

    build

--- docs/assets/chevron-down.53d97774.js
+++
@@ -1,1 +0,0 @@
-import{b as e,c as o,e as t}from"./index.e6b7318f.js";const r={xmlns:"http://www.w3.org/2000/svg",viewBox:"0 0 24 24"},s=t("path",{d:"M7.41 8.58 12 13.17l4.59-4.59L18 10l-6 6-6-6 1.41-1.42Z"},null,-1),c=[s];function n(a,l){return e(),o("svg",r,c)}var d={render:n};export{d as A};
