6126c2bf6129cdfbd169a5a97108e14efab9b9f6
ivan.turovsky <ivan.turovsky@superbbit.com>
2023-10-26 11:48:59 +0300

    build

---
+++ docs/assets/1.de3281ab.js
@@ -0,0 +1,1 @@
+var a="/vue-crm-materio/assets/1.aac046b6.png";export{a as _};
---
+++ docs/assets/VSplitter.d7b3c171.js
@@ -0,0 +1,1 @@
+import{_ as o,b as e,c as r,n as t}from"./index.ffeb54e1.js";const c="_root_3klhz_1";var l={root:c};const n={};function a(s,i){return e(),r("div",{class:t(s.$style.root)},null,2)}const _={$style:l};var f=o(n,[["render",a],["__cssModules",_]]);export{f as V};
